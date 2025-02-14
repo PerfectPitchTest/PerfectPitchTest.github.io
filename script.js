2940260a4e4e9b1a8bde5235963a3ff05a678543
itamartmm21 <itusht21@gmail.com>
2025-02-14 17:03:28 +0200

    initial release

---
+++ script.js
@@ -0,0 +1,113 @@
+const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+let notesAudio = [];
+let chosenNote;
+let writtenNote;
+let difficalty = 11;
+let lastNote;
+let lastWrittenNote;
+let score = 0;
+let isRunning = false;
+const element = document.getElementsByClassName("Question");
+let seconds = 45;
+let randomIndex;
+const timerElement = document.querySelector(".timer");
+const timerOffColor = "#999898"
+const timerOnColor = "#e9e7e7"
+for(let i=0; i<notes.length; i++){
+    let currentNote = notes[i]
+    notesAudio[i] = new Audio(currentNote.replace('#', ' sharp') + ".mp3");
+    notesAudio[i].preload = "auto";
+}
+
+timerElement.style.color = timerOffColor;
+function selectNote(){
+    randomIndex = Math.floor(Math.random() * (difficalty + 1));
+    while (notes[randomIndex] == lastWrittenNote || notes[randomIndex] == lastNote){
+        randomIndex = Math.floor(Math.random() * (difficalty + 1));
+    }
+    chosenNote = notes[randomIndex];
+    writtenNote = chosenNote;
+    lastWrittenNote = writtenNote;
+    element[0].textContent = 'Is this the note ' + chosenNote + '?';
+    if (Math.floor(Math.random() * 2) == 0){
+        randomIndex = Math.floor(Math.random() * (difficalty + 1));
+        while (notes[randomIndex] == chosenNote || notes[randomIndex] == lastNote){
+            randomIndex = Math.floor(Math.random() * (difficalty + 1));
+        }
+        chosenNote = notes[randomIndex];
+    }
+    lastNote = chosenNote;
+    console.log(chosenNote);
+}
+
+function playNote(pressed) {
+    if (!isRunning && pressed) {
+        seconds = 45;
+        isRunning = true;
+        timerElement.style.color = timerOnColor;
+        selectNote();
+    }
+    notesAudio[randomIndex].currentTime = 0;
+
+    notesAudio[randomIndex].play();
+}
+
+
+function isNote(bool) {
+    if(!isRunning)
+        return;
+    const element = document.getElementsByClassName("timer")[0];
+    const correctColor = "rgb(90, 179, 90)";
+    const wrongColor = "rgb(226, 78, 78)";
+
+    // Change the background color based on the answer
+    if ((writtenNote == chosenNote) == bool) {
+        element.style.color = correctColor;  // Correct answer
+        seconds += 5;
+    } else {
+        element.style.color = wrongColor;  // Wrong answer
+        seconds -= 6;
+    }
+
+    setTimeout(() => {
+        element.style.color = isRunning ? timerOnColor : timerOffColor;
+    }, 200);
+    
+
+    selectNote();
+    playNote(false);
+    
+}
+
+
+
+function updateTimer() {
+    // Increment seconds
+    if(isRunning){
+        seconds -= 0.2;
+
+        if(seconds <= 0){
+            seconds = 0;
+            isRunning = false;
+            timerElement.style.color = timerOffColor;
+            element[0].textContent = "Time's up! Press the gray button to restart";
+        }
+    }
+    // Convert seconds to minutes and seconds
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds) % 60;
+
+    // Format the time as MM:SS
+    const timeFormatted = `${minutes}:${remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds}`;
+
+    // Update the text content of the timer element
+    timerElement.textContent = timeFormatted;
+
+    // Stop the timer after 60 seconds
+    if (seconds === 60) {
+        clearInterval(timerInterval);
+    }
+}
+
+// Start the timer
+const timerInterval = setInterval(updateTimer, 200); // Update every second (1000ms)
