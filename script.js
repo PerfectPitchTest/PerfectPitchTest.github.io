9088a621bba9e8983e0a8138b10e9f9638570dde
itamartmm21 <itusht21@gmail.com>
2025-05-02 12:10:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ function updateTimer() {
             seconds = 0;
             isRunning = false;
             timerElement.style.color = timerOffColor;
-            element[0].innerHTML = `Time's up!<br>Your score is ${score}. Press the note button to play again.`;
+            element[0].innerHTML = `Your score is ${score}.<br>Press the note button to play again.`;
             score = 0;
         }
     }
